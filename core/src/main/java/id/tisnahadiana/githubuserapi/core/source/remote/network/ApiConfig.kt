f247aacf7c5c7e7f408fe77dcbbf6801ea078a53
Tisna Hadiana <tisnahadiana@gmail.com>
2023-10-22 14:54:07 +0700

    Adding certificate pinning

--- core/src/main/java/id/tisnahadiana/githubuserapi/core/source/remote/network/ApiConfig.kt
+++ core/src/main/java/id/tisnahadiana/githubuserapi/core/source/remote/network/ApiConfig.kt
@@ -1,5 +1,6 @@
 package id.tisnahadiana.githubuserapi.core.source.remote.network
 
+import okhttp3.CertificatePinner
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
@@ -15,8 +16,16 @@ object ApiConfig {
                 .build()
             chain.proceed(requestHeaders)
         }
+        val hostname = "api.github.com"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/jFaeVpA8UQuidlJkkpIdq3MPwD0m8XbuCRbJlezysBE=")
+            .add(hostname, "sha256/Wec45nQiFwKvHtuHxSAMGkt19k+uPSw9JlEkxhvYPHk=")
+            .add(hostname, "sha256/Jg78dOE+fydIGk19swWwiypUSR6HWZybfnJG/8G7pyM=")
+            .build()
+
         val client = OkHttpClient.Builder()
             .addInterceptor(authInterceptor)
+            .certificatePinner(certificatePinner)
             .build()
 
         return Retrofit.Builder()
