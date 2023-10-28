951e77af1182b00dd250b6f86ded5ad5f1561673
Tisna Hadiana <tisnahadiana@gmail.com>
2023-10-28 10:34:47 +0700

    Fix performance issue

--- app/src/main/java/id/tisnahadiana/githubuserapi/main/SplashActivity.kt
+++ app/src/main/java/id/tisnahadiana/githubuserapi/main/SplashActivity.kt
@@ -1,5 +1,6 @@
 package id.tisnahadiana.githubuserapi.main
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +8,7 @@ import android.os.Handler
 import android.os.Looper
 import id.tisnahadiana.githubuserapi.R
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
 
     private val SPLASH_DELAY_MS = 2000L
